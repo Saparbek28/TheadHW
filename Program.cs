d5618a4dd5148a7fd3fab339ebe9993f3a53fc2e
Saparbek Bainazar <Saparbek28@gmail.com>
2019-05-30 18:02:02 +0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+using System.Threading;
+using System.IO;
+
+namespace ThreadHW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Thread PlayMusic = new Thread(Music);
+            PlayMusic.IsBackground = true;
+            Text();
+            PlayMusic.Start();
+
+        }
+
+        static void Music()
+        {
+            SoundPlayer soundPlayer = new SoundPlayer(@"C:\Users\БайназарС.CORP\source\repos\ThreadHW\rammstein_-_deutschland.wav");
+            soundPlayer.Play();
+        }
+
+        static void Text()
+        {
+            string write = @"C:\Users\БайназарС.CORP\source\repos\ThreadHW\qwe.txt";
+            string text = Console.ReadLine();
+            using(StreamWriter writer = new StreamWriter(write, false, System.Text.Encoding.Default))
+            {
+                writer.WriteLine(text);
+            }
+        }
+    }
+}
